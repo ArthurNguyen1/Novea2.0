c41fc4b693dfc3cd5aade5d87815782d3122cfbb
nhvtmanh <21522329@gm.uit.edu.vn>
2023-04-30 22:39:03 +0700

    switch login user type

--- Novea2.0/ViewModel/Login/MainLoginViewModel.cs
+++ Novea2.0/ViewModel/Login/MainLoginViewModel.cs
@@ -1,13 +1,45 @@
-﻿using System;
+﻿using Novea2._0.View;
+using Novea2._0.View.Login;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Novea2._0.ViewModel.Login
 {
     public class MainLoginViewModel : BaseViewModel
     {
+        public ICommand SwitchTab { get; set; }
+        public ICommand GetIdTab { get; set; }
+        int buttonIndex;
+        public MainLoginViewModel()
+        {
+            GetIdTab = new RelayCommand<Button>((p) => true, (p) => buttonIndex = int.Parse(p.Uid));
+            SwitchTab = new RelayCommand<MainLogin>((p) => true, (p) => switchTab(p));
+        }
 
+        private void switchTab(MainLogin p)
+        {
+            switch (buttonIndex)
+            {
+                case 0:
+                    p.LoginFrame.NavigationService.Navigate(new AdminLogin());
+                    break;
+                case 1:
+                    p.LoginFrame.NavigationService.Navigate(new ClientLogin());
+                    break;
+                case 2:
+                    p.LoginFrame.NavigationService.Navigate(new StoreLogin());
+                    break;
+                case 3:
+                    p.LoginFrame.NavigationService.Navigate(new ShipperLogin());
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
