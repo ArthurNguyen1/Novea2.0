5aceecb6e7200a2c0e75393174f2f199fd0efe87
nhvtmanh <21522329@gm.uit.edu.vn>
2023-04-30 22:10:52 +0700

    add StoreLogin

---
+++ Novea2.0/ViewModel/Login/StoreLoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novea2._0.ViewModel.Login
+{
+    public class StoreLoginViewModel : BaseViewModel
+    {
+
+    }
+}
