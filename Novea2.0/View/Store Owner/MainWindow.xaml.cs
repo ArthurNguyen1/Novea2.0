54c89302fdd328f9da3b9cf249145eccf61e21fc
nhvtmanh <21522329@gm.uit.edu.vn>
2023-05-01 14:47:27 +0700

    add MainWindow of Store Owner

--- Novea2.0/View/Store Owner/MainWindow.xaml.cs
+++ Novea2.0/View/Store Owner/MainWindow.xaml.cs
@@ -10,10 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Novea2._0
+namespace Novea2._0.View.Store_Owner
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
