2e6e5350821c70ecbc0a5f96b651c413df5afe71
Minh Nguyễn Hải <21522342@ms.uit.edu.vn>
2023-05-05 19:37:17 +0700

    Create entities

---
+++ Novea2.0/Model/DataProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novea2._0.Model
+{
+    public class DataProvider
+    {
+        private static DataProvider _ins;
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_ins == null)
+                    _ins = new DataProvider();
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+            }
+        }
+
+        public QuanLyBanHangNoveaEntities DB { get; set; }
+
+        private DataProvider()
+        {
+            DB = new QuanLyBanHangNoveaEntities();
+        }
+        public void Refresh()
+        {
+            DB = new QuanLyBanHangNoveaEntities();
+        }
+    }
+}
