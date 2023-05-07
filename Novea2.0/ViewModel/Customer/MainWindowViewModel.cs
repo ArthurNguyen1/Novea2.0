e1dd5f950afa2bb5887eef673e386730599783c0
nhvtmanh <21522329@gm.uit.edu.vn>
2023-05-07 21:59:55 +0700

    add Cart of Customer

--- Novea2.0/ViewModel/Customer/MainWindowViewModel.cs
+++ Novea2.0/ViewModel/Customer/MainWindowViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Novea2._0.ViewModel.Customer
@@ -15,11 +16,17 @@ namespace Novea2._0.ViewModel.Customer
         public ICommand CloseWdCommand { get; set; }
         public ICommand MinimizeWdCommand { get; set; }
         public ICommand LogOutCommand { get; set; }
+        public ICommand SwitchTabCommand { get; set; }
+        public ICommand GetIdTab { get; set; }
+
+        private int buttonIndex;
         public MainWindowViewModel()
         {
             CloseWdCommand = new RelayCommand<MainWindow>((p) => true, (p) => CloseWd());
             MinimizeWdCommand = new RelayCommand<MainWindow>((p) => true, (p) => MinimizeWd(p));
             LogOutCommand = new RelayCommand<MainWindow>((p) => true, (p) => LogOut(p));
+            GetIdTab = new RelayCommand<RadioButton>((p) => true, (p) => buttonIndex = int.Parse(p.Uid));
+            SwitchTabCommand = new RelayCommand<MainWindow>((p) => true, (p) => SwitchTab(p));
         }
         private void CloseWd()
         {
@@ -34,6 +41,24 @@ namespace Novea2._0.ViewModel.Customer
             MainLogin mainLogin = new MainLogin();
             mainLogin.Show();
             p.Close();
+        }
+        private void SwitchTab(MainWindow p)
+        {
+            switch (buttonIndex)
+            {
+                case 0:
+                    p.MainFrame.NavigationService.Navigate(new Home());
+                    break;
+                case 1:
+                    p.MainFrame.NavigationService.Navigate(new Cart());
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
