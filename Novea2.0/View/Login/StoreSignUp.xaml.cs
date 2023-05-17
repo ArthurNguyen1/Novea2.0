20523270c9f804726b80ac7fdb62fe2e50ed82f8
nhvtmanh <21522329@gm.uit.edu.vn>
2023-05-17 22:21:30 +0700

    add StoreSignUp

---
+++ Novea2.0/View/Login/StoreSignUp.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Novea2._0.View.Login
+{
+    /// <summary>
+    /// Interaction logic for StoreSignUp.xaml
+    /// </summary>
+    public partial class StoreSignUp : Window
+    {
+        public StoreSignUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
