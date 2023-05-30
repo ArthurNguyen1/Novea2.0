0a5a3a6a22a7a3908f07e79a50d66c708c0a2d09
nhvtmanh <21522329@gm.uit.edu.vn>
2023-05-30 22:12:00 +0700

    move base64encode and md5hash function to MainLoginViewModel

--- Novea2.0/ViewModel/Login/ClientLoginViewModel.cs
+++ Novea2.0/ViewModel/Login/ClientLoginViewModel.cs
@@ -49,7 +49,7 @@ namespace Novea2._0.ViewModel.Login
             try
             {
                 if (p == null) return;
-                string PassEncode = MD5Hash(Base64Encode(Password));
+                string PassEncode = MainLoginViewModel.MD5Hash(MainLoginViewModel.Base64Encode(Password));
                 var accCountKH = DataProvider.Ins.DB.KHACHes.Where(x => x.TAIKHOAN == Username && x.MATKHAU == PassEncode && x.STATU == true).Count();
                 if (accCountKH > 0)
                 {
@@ -81,23 +81,6 @@ namespace Novea2._0.ViewModel.Login
             {
                 MessageBox.Show("Mất kết nối đến cơ sở dữ liệu!", "Thông báo", MessageBoxButton.OK);
             }
-        }
-        public static string Base64Encode(string plainText)
-        {
-            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
-            return System.Convert.ToBase64String(plainTextBytes);
-        }
-        public static string MD5Hash(string input)
-        {
-            StringBuilder hash = new StringBuilder();
-            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
-            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
-
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                hash.Append(bytes[i].ToString("x2"));
-            }
-            return hash.ToString();
         }
         //void _RegisterCommand(AdminLoginPage parameter)
         //{
--- Novea2.0/ViewModel/Login/MainLoginViewModel.cs
+++ Novea2.0/ViewModel/Login/MainLoginViewModel.cs
@@ -13,6 +13,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using Xamarin.Forms.PlatformConfiguration;
+using System.Security.Cryptography;
 
 namespace Novea2._0.ViewModel.Login
 {
@@ -67,6 +68,23 @@ namespace Novea2._0.ViewModel.Login
                 default:
                     break;
             }
+        }
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+        public static string MD5Hash(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
+            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            return hash.ToString();
         }
     }
 }
