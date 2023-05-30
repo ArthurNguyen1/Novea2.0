86c4941f0c67ec28de269b54a597db373e46c281
nhvtmanh <21522329@gm.uit.edu.vn>
2023-05-30 22:58:57 +0700

    add CloseWindow in ForgotPassword

--- Novea2.0/ViewModel/Login/ForgotPasswordViewModel.cs
+++ Novea2.0/ViewModel/Login/ForgotPasswordViewModel.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using Novea2._0.View.Login;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Novea2._0.ViewModel.Login
 {
     public class ForgotPasswordViewModel : BaseViewModel
     {
-
+        public ICommand CloseWdCommand { get; set; }
+        public ForgotPasswordViewModel()
+        {
+            CloseWdCommand = new RelayCommand<ForgotPassword>((p) => true, (p) => CloseWindow(p));
+        }
+        void CloseWindow(ForgotPassword p)
+        {
+            p.Close();
+        }
     }
 }
