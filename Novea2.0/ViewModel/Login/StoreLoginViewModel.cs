d281f8186a1d210cdf2084c1abd35a4aa4e89a0a
nhvtmanh <21522329@gm.uit.edu.vn>
2023-05-31 08:33:42 +0700

    delete unneeded code in StoreLogin

--- Novea2.0/ViewModel/Login/StoreLoginViewModel.cs
+++ Novea2.0/ViewModel/Login/StoreLoginViewModel.cs
@@ -18,12 +18,7 @@ namespace Novea2._0.ViewModel.Login
 {
     public class StoreLoginViewModel : BaseViewModel
     {
-        private string _Username;
-        public string Username { get => _Username; set { _Username = value; OnPropertyChanged(); } }
-        private string _Password;
-        public string Password { get => _Password; set { _Password = value; OnPropertyChanged(); } }
         public ICommand Login { get; set; }
-        public ICommand PasswordChangedCommand { get; set; }
         public ICommand RegisterCommand { get; set; }
         public ICommand ForgetPassCommand { get; set; }
         public ICommand _Loadwd { get; set; }
@@ -31,11 +26,8 @@ namespace Novea2._0.ViewModel.Login
         public StoreLoginViewModel()
         {
             Const.IsLogin = false;
-            Password = "";
-            Username = "";
             _Loadwd = new RelayCommand<StoreLogin>((p) => true, (p) => loadwd());
             Login = new RelayCommand<StoreLogin>((p) => true, (p) => login(p));
-            PasswordChangedCommand = new RelayCommand<PasswordBox>((p) => true, (p) => { Password = p.Password; });
             RegisterCommand = new RelayCommand<StoreLogin>((p) => true, (p) => Register());
             ForgetPassCommand = new RelayCommand<StoreLogin>((p) => true, (p) => ForgetPass());
         }
@@ -81,23 +73,6 @@ namespace Novea2._0.ViewModel.Login
             {
                 MessageBox.Show("Mất kết nối đến cơ sở dữ liệu!", "Thông báo", MessageBoxButton.OK);
             }
-        }
-        public static string Base64Encode(string plainText)
-        {
-            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
-            return System.Convert.ToBase64String(plainTextBytes);
-        }
-        public static string MD5Hash(string input)
-        {
-            StringBuilder hash = new StringBuilder();
-            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
-            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
-
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                hash.Append(bytes[i].ToString("x2"));
-            }
-            return hash.ToString();
         }
         void Register()
         {
