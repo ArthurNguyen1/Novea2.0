599b28ed5ba419cae6555ed4250ddfab65d3b15e
nhvtmanh <21522329@gm.uit.edu.vn>
2023-06-02 23:16:20 +0700

    add remember account function

--- Novea2.0/ViewModel/Login/StoreLoginViewModel.cs
+++ Novea2.0/ViewModel/Login/StoreLoginViewModel.cs
@@ -22,49 +22,69 @@ namespace Novea2._0.ViewModel.Login
         public ICommand RegisterCommand { get; set; }
         public ICommand ForgetPassCommand { get; set; }
         public ICommand _Loadwd { get; set; }
-
+        public ICommand UncheckedCommand { get; set; }
         public StoreLoginViewModel()
         {
             Const.IsLogin = false;
-            _Loadwd = new RelayCommand<StoreLogin>((p) => true, (p) => loadwd());
+            _Loadwd = new RelayCommand<StoreLogin>((p) => true, (p) => loadwd(p));
             Login = new RelayCommand<StoreLogin>((p) => true, (p) => login(p));
             RegisterCommand = new RelayCommand<StoreLogin>((p) => true, (p) => Register());
             ForgetPassCommand = new RelayCommand<StoreLogin>((p) => true, (p) => ForgetPass());
+            UncheckedCommand = new RelayCommand<CheckBox>((p) => true, (p) => Unchecked());
         }
-        void loadwd()
+        void Unchecked()
+        {
+            Properties.Settings.Default.isRememberCheck = false;
+            Properties.Settings.Default.Save();
+        }
+        void loadwd(StoreLogin p)
         {
             Const.IsLogin = false;
+            if (Properties.Settings.Default.isRememberCheck == true)
+            {
+                p.tbUsername.Text = Properties.Settings.Default.username;
+                p.password.Password = Properties.Settings.Default.password;
+                p.Remember.IsChecked = true;
+            }
         }
-
         public void login(StoreLogin p)
         {
             try
             {
                 if (p == null) return;
                 string username = p.tbUsername.Text;
-                string PassEncode = MainLoginViewModel.MD5Hash(MainLoginViewModel.Base64Encode(p.password.Password));
-                var accCountCH = DataProvider.Ins.DB.CUAHANGs.Where(x => x.TAIKHOAN == username && x.MATKHAU == PassEncode && x.STATU == true).Count();
-                if (accCountCH > 0)
+                string PassEncode = MainLoginViewModel.MD5Hash(MainLoginViewModel.Base64Encode(p.password.Password));               
+                foreach (CUAHANG store in DataProvider.Ins.DB.CUAHANGs)
                 {
-                    if (p.Remember.IsChecked == true)
+                    if (username == store.TAIKHOAN && PassEncode == store.MATKHAU)
                     {
-                        Const.IsLogin = true;
-                        Const.TenDangNhap = username;
-
-                        Properties.Settings.Default.Save();
-
-                        MainWindow mainWindow = new MainWindow();
-                        mainWindow.Show();
-                    }
-                    else
-                    {
-                        Const.IsLogin = true;
-                        Const.TenDangNhap = username;
-                        MainWindow mainWindow = new MainWindow();
-                        mainWindow.Show();
+                        if (store.STATU == true)
+                        {
+                            if (p.Remember.IsChecked == true)
+                            {
+                                Properties.Settings.Default.isRememberCheck = true;
+                                Properties.Settings.Default.username = username;
+                                Properties.Settings.Default.password = p.password.Password;
+                                Properties.Settings.Default.Save();
+                            }
+                            if (p.Remember.IsChecked == false)
+                            {
+                                Properties.Settings.Default.isRememberCheck = false;
+                                Properties.Settings.Default.Save();
+                            }
+                            Const.IsLogin = true;
+                            Const.CH = store;
+                            MainWindow mainWindow = new MainWindow();
+                            mainWindow.Show();
+                            return;
+                        }
+                        else
+                        {
+                            //Process blocked account
+                        }
                     }
                 }
-                else
+                if (Const.IsLogin == false)
                 {
                     MessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng!", "Thông báo", MessageBoxButton.OK);
                 }
