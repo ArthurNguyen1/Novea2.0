52dfd31b23b6d636b49e383466ec9b183b61f695
nhvtmanh <21522329@gm.uit.edu.vn>
2023-06-06 21:23:02 +0700

    add AdminSignUp UI

--- Novea2.0/ViewModel/Login/AdminLoginViewModel.cs
+++ Novea2.0/ViewModel/Login/AdminLoginViewModel.cs
@@ -1,13 +1,31 @@
-﻿using System;
+﻿using Novea2._0.View.Login;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Novea2._0.ViewModel.Login
 {
     public class AdminLoginViewModel : BaseViewModel
     {
-
+        public ICommand RegisterCommand { get; set; }
+        public ICommand ForgetPassCommand { get; set; }
+        public AdminLoginViewModel()
+        {
+            RegisterCommand = new RelayCommand<AdminLogin>((p) => true, (p) => Register());
+            ForgetPassCommand = new RelayCommand<AdminLogin>((p) => true, (p) => ForgetPass());
+        }
+        void ForgetPass()
+        {
+            ForgotPassword forgotPassword = new ForgotPassword();
+            forgotPassword.ShowDialog();
+        }
+        void Register()
+        {
+            AdminSignUp adminSignUp = new AdminSignUp();
+            adminSignUp.ShowDialog();
+        }
     }
 }
