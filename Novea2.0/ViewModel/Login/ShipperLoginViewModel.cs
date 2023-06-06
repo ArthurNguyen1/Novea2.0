016be023f5d2c4c5bb1a7b5c616784011e94c7cc
nhvtmanh <21522329@gm.uit.edu.vn>
2023-06-06 21:38:53 +0700

    add ShipperSignUp UI

--- Novea2.0/ViewModel/Login/ShipperLoginViewModel.cs
+++ Novea2.0/ViewModel/Login/ShipperLoginViewModel.cs
@@ -1,13 +1,31 @@
-﻿using System;
+﻿using Novea2._0.View.Login;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Novea2._0.ViewModel.Login
 {
     public class ShipperLoginViewModel : BaseViewModel
     {
-
+        public ICommand RegisterCommand { get; set; }
+        public ICommand ForgetPassCommand { get; set; }
+        public ShipperLoginViewModel()
+        {
+            RegisterCommand = new RelayCommand<ShipperLogin>((p) => true, (p) => Register());
+            ForgetPassCommand = new RelayCommand<ShipperLogin>((p) => true, (p) => ForgetPass());
+        }
+        void ForgetPass()
+        {
+            ForgotPassword forgotPassword = new ForgotPassword();
+            forgotPassword.ShowDialog();
+        }
+        void Register()
+        {
+            ShipperSignUp shipperSignUp = new ShipperSignUp();
+            shipperSignUp.ShowDialog();
+        }
     }
 }
