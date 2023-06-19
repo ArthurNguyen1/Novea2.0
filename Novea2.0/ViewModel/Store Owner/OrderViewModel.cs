03c62eea9386080f4ab5090bd8995d802330c4a1
nhvtmanh <21522329@gm.uit.edu.vn>
2023-06-19 15:34:19 +0700

    fix Order of Store owner

--- Novea2.0/ViewModel/Store Owner/OrderViewModel.cs
+++ Novea2.0/ViewModel/Store Owner/OrderViewModel.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
 
 namespace Novea2._0.ViewModel.Store_Owner
@@ -22,11 +23,13 @@ namespace Novea2._0.ViewModel.Store_Owner
         public ICommand Detail { get; set; }
         public ICommand LoadCsCommand { get; set; }
         public ICommand FilterCommand { get; set; }
+        public ICommand SearchCommand { get; set; }
         public OrderViewModel()
         {
             FilterCommand = new RelayCommand<Order>((p) => true, (p) => Filter(p));
             Detail = new RelayCommand<Order>((p) => p.ListViewHD.SelectedItem != null ? true : false, (p) => _Detail(p));
             LoadCsCommand = new RelayCommand<Order>((p) => true, (p) => _LoadCsCommand(p));
+            SearchCommand = new RelayCommand<Order>((p) => true, (p) => Search(p));
         }
         void _LoadCsCommand(Order parameter)
         {
@@ -37,7 +40,6 @@ namespace Novea2._0.ViewModel.Store_Owner
             Filter(parameter);
 
         }
-
         void Filter(Order p)
         {
             switch (p.cbbFilter.SelectedIndex.ToString())
@@ -68,7 +70,6 @@ namespace Novea2._0.ViewModel.Store_Owner
                     }
             }
         }
-
         void _Detail(Order parameter)
         {
             DetailOrder detailOrder = new DetailOrder();
@@ -78,11 +79,38 @@ namespace Novea2._0.ViewModel.Store_Owner
             detailOrder.TenKH.Text = temp.KHACH.HOTEN;
             detailOrder.DiaChi.Text = temp.KHACH.DIACHI;
             detailOrder.SDT.Text = temp.KHACH.SDT;
+            if (temp.STATU == "Đang xử lý")
+            {
+                detailOrder.btConfirm.Visibility = System.Windows.Visibility.Visible;
+            }
             detailOrder.ShowDialog();
             parameter.ListViewHD.SelectedItem = null;
             listHD1 = new ObservableCollection<HOADON>(DataProvider.Ins.DB.HOADONs.Where(h => h.MACH == Const.CH.MACH && h.STATU != "Khởi tạo"));
             listHD = new ObservableCollection<HOADON>(listHD1.GroupBy(h => h.SOHD).Select(grp => grp.FirstOrDefault()));
             parameter.ListViewHD.ItemsSource = listHD;
+            parameter.cbbFilter.SelectedIndex = 0;
+        }
+        void Search(Order p)
+        {
+            ObservableCollection<HOADON> temp = new ObservableCollection<HOADON>();
+            if (p.txbSearch.Text == "")
+            {
+                p.ListViewHD.ItemsSource = listHD;
+            }
+            else
+            {
+                foreach (HOADON h in listHD)
+                {
+                    if (h.SOHD.ToLower().Contains(p.txbSearch.Text.ToLower()))
+                    {
+                        temp.Add(h);
+                    }
+                }
+                if (temp != null)
+                {
+                    p.ListViewHD.ItemsSource = temp;
+                }
+            }
         }
     }
 }
