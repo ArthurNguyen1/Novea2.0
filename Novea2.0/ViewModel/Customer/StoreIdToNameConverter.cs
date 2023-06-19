57478a6c0d09e37a76c61dab537e710d20e8eed9
nhvtmanh <21522329@gm.uit.edu.vn>
2023-06-19 16:31:50 +0700

    fix not display store name in OrderHistory of Customer

---
+++ Novea2.0/ViewModel/Customer/StoreIdToNameConverter.cs
@@ -0,0 +1,30 @@
+﻿using Novea2._0.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Novea2._0.ViewModel.Customer
+{
+    public class StoreIdToNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string storeId = (string)value;
+            var store = DataProvider.Ins.DB.CUAHANGs.FirstOrDefault(s => s.MACH == storeId);
+            if (store != null)
+            {
+                return store.TENCH;
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
